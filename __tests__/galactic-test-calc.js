4a1b8e3109ff999afba5625e58b7914f31039b15
basicjohn <edmondsonj@gmail.com>
2021-04-16 08:51:46 -0700

    add new good fail. Trouble with beforeEach()

--- __tests__/galactic-test-calc.js
+++ __tests__/galactic-test-calc.js
@@ -2,12 +2,9 @@ import AgeCalculator from './../src/js/galactic-calc.js';
 
 
 describe('AgeCalculator', () => {
-  beforeEach(() => {
-    const ageCalculator = new AgeCalculator;
-  });
 
   test('Create the ageCalculator object using the AgeCalculator constructor', () => {
-
+    const ageCalculator = new AgedCalculator;
     expect(ageCalculator);
   });
 });
--- src/js/galactic-calc.js
+++ src/js/galactic-calc.js
@@ -1,4 +1,7 @@
 export default class AgeCalculator {
-
-
+  constructor(){
+    this.earthAge = 0;
+    this.averageEarthAge = 85;
+    this.planets = [];
+  }
 }
