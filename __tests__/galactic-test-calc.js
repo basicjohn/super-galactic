c132945ba5c879c56c4df7047e946e08613be732
basicjohn <edmondsonj@gmail.com>
2021-04-16 08:57:44 -0700

    good fail second test

--- __tests__/galactic-test-calc.js
+++ __tests__/galactic-test-calc.js
@@ -4,7 +4,12 @@ import AgeCalculator from './../src/js/galactic-calc.js';
 describe('AgeCalculator', () => {
 
   test('Create the ageCalculator object using the AgeCalculator constructor', () => {
-    const ageCalculator = new AgedCalculator;
+    const ageCalculator = new AgeCalculator;
     expect(ageCalculator);
+  });
+
+  test('Check averageEarthAge to make sure object is created', () => {
+    const ageCalculator = new AgeCalculator;
+    expect(ageCalculator.averageEarthAge).toEqual(90);
   });
 });
