6a827cc53376d28fc36aacaadfb6fdb65a2bff6c
basicjohn <edmondsonj@gmail.com>
2021-04-16 10:14:15 -0700

    recheck good fail for test 5

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -2,9 +2,9 @@ import {populate} from './../src/main.js';
 
 describe('populate', () => {
 
-  test('Create the planet object using the Planet constructor', () => {
-    populate();
-    expect(mars.name).toMatch("Mars");
+  test('Create the planet object using the Planet constructor & store in ageCalculator', () => {
+    const ageCalculator = populate();
+    expect(ageCalculator.planets[0].name).toMatch("Europa");
   });
 
 });
