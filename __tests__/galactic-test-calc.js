cf7fbea0d8326d93c699ace434fe39a1142d90a5
basicjohn <edmondsonj@gmail.com>
2021-04-16 09:07:26 -0700

    good fail 3rd test

--- __tests__/galactic-test-calc.js
+++ __tests__/galactic-test-calc.js
@@ -12,4 +12,14 @@ describe('AgeCalculator', () => {
     const ageCalculator = new AgeCalculator;
     expect(ageCalculator.averageEarthAge).toEqual(85);
   });
+
+  test('create planet and add it to AgeCalculator.planets', () => {
+    const ageCalculator = new AgeCalculator;
+    const planetName = "Mars";
+    const planetYear =  1.88;
+    AgeCalculator.newPlanet("Mars", 1.88);
+    expect(AgeCalculator.planets[0].name).toEqual(planetName);
+    expect(AgeCalculator.planets[0].yearLength).toEqual(planetYear);
+
+  });
 });
